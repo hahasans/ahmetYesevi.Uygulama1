b634ac5d14a8b283353be8d6ce24292ef41bff90
hahasans <www.hasansahin.com@hotmail.com>
2016-11-28 23:11:51 +0300

    Mobile1

---
+++ Uygulama.Mobile/Uygulama.Mobile.UI/Uygulama.Mobile.UI/Uygulama.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uygulama.Mobile.BLL;
+using Xamarin.Forms;
+
+namespace Uygulama.Mobile.UI
+{
+    public partial class Uygulama : ContentPage
+    {
+        public Uygulama()
+        {
+            InitializeComponent();
+            List();
+        }
+        private void List()
+        {
+            var uygulamaBll=new UygulamaBLL();
+            lstUygulama.BindingContext = uygulamaBll.List();
+        }
+
+        private void OnRefresh(object sender, EventArgs e)
+        {
+            List();
+            lstUygulama.IsRefreshing = false;
+        }
+    }
+}
